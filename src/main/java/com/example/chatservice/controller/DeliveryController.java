0739200b85ac5e0e62392d974a796209f54c4770
dlstlr1707 <dlstlr1707@naver.com>
2025-05-15 18:16:13 +0900

    25/05/15

--- src/main/java/com/example/chatservice/controller/DeliveryController.java
+++ src/main/java/com/example/chatservice/controller/DeliveryController.java
@@ -4,15 +4,17 @@ import com.example.chatservice.dto.DeliveryLocationDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class DeliveryController {
-    @MessageMapping("/delivery/update-location") // 클라이언트 -> 서버
-    @SendTo("/topic/delivery-location") // 서버 -> 구독자
-    public DeliveryLocationDTO handleLocationUpdate(DeliveryLocationDTO location) {
-        // 여기에 로직이 있으면 가공 가능
-        return location; // 이 데이터가 /topic/delivery-location 로 브로드캐스팅됨
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/delivery/update-location") // 클라이언트 → 서버
+    public void handleLocationUpdate(DeliveryLocationDTO location) {
+        String topic = "/topic/delivery-location/" + location.getMerchantUid(); // 동적 토픽 생성
+        messagingTemplate.convertAndSend(topic, location); // 해당 토픽으로 전송
     }
 }
